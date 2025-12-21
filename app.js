054124cc18d43a69478b7b672f2d3d4a6b45b93f
Arafat Patwary <farabipsk@gmail.com>
2025-12-21 11:28:06 +0600

    Add new os project

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const math = require('./math.js');
+const http = require('http');
+const os = require('os');
+const process = require('process');
+const url = require('url');
 
 
-const res1= math.add(11,22);
-const res2 =math.sub(33,11);
-console.log(res1);
+//? Formate byte to human-readable format 
+function formatBytes(bytes, decimal =2){
+    if(bytes ===0) return '0 bytes'
+
+    //set base unit
+    const k = 1024
+    const size = ['Bytes','KB','MB','TB','PB']
+    const i = Math.floor(Math.log(bytes)/Math.log(k))
+    return parseFloat((bytes/Math.pow(k,i))).toFixed(decimal) + ' ' + size[i]
+}
+
+
+//get memory info
+//get os info
+//get user info
+//get network info
+//get process
+console.log(os.cpus())
--- practice/server_routing.js
+++ practice/server_routing.js
@@ -1,21 +1,57 @@
-const http = require('http');
+const http = require("http");
 //create server
-const server = http.createServer((req,res)=>{
-    //log details
-    console.log(`Received ${req.method} request for: ${req.url}`);
+const server = http.createServer((req, res) => {
+  //log details
+  console.log(`Received ${req.method} request for: ${req.url}`);
 
-    //set code 
-    res.writeHead(200, {"Content-Type":"text/plain"});
+  //set code
+  res.writeHead(200, { "Content-Type": "text/plain" });
 
-    //set status
+  //set status
+//   if(req.url==='/' && req.method==='GET'){
+//     res.statusCode= 200;
+//     res.end('Welcome to home page')
+//   }else if(req.url === '/about' && req.method==='GET'){
+//      res.statusCode= 200;
+//     res.end('Welcome to About page')
+//   }else if(req.url === '/contact' && req.method==='GET'){
+//      res.statusCode= 200;
+//     res.end('Contact us at @yahoo mail')
+//   }else{
+//      res.statusCode= 404;
+//     res.end('404-page not found')
+//   }
 
-    res.end('hello server, first node')
+ // res.end("hello server, first node");
+//});
+
+//! JSON data
+if(req.url==='/' && req.method==='GET'){
+    res.statusCode= 200;
+    res.end(JSON.stringify({message: 'Welcome to JSON data Api'}))
+}else if(req.url==='/users' && req.method==='GET'){
+    res.statusCode= 200;
+    const usersData= [{id: 1, name: 'Smith', role: 'Developer',
+        id: 2, name: 'Gam', role: 'Engineer',
+        id: 3, name: 'Lohan', role: 'AI',
+    }]
+    res.end(JSON.stringify(usersData))
+}else if(req.url==='/products' && req.method==='GET'){
+    res.statusCode= 200;
+    const productData=[{
+        id: 11, name: 'Honey', price: 234,
+        id: 22, name: 'Nut', price: 543,
+        id: 33, name: 'Chees', price: 154
+    }]
+    res.end(JSON.stringify(productData))
+}else{
+    res.end('404-page not found');
+}
 })
-
 //?Define port
 
 const PORT = 7000;
-server.listen(PORT, ()=>{
-    console.log(`Server  running at: http://localhost:${PORT}`);
-    console.log('Press ctrl+ c to stop server');
-})
+server.listen(PORT, () => {
+  console.log(`Server  running at: http://localhost:${PORT}`);
+  console.log("Press ctrl+ c to stop server");
+});
